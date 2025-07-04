e052e16da9f30ec511af2ec49eacd1ea429a778c
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2025-07-04 11:52:28 -0300

    Delete dist directory

--- dist/auth.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const handleLogin: (company: string, username: string, password: string, userType: string) => Promise<any>;
-export declare const refreshToken: () => Promise<any>;
-export declare const recoverPassword: (username: string, email: string, company: string) => Promise<any>;
-export declare const getUserPermissions: () => Promise<any>;
