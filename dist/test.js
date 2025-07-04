24f74c4ed466a6dab00de00f0e47cfee8d7b7fe4
Kevyn <kevwsantos9@gmail.com>
2025-07-04 11:15:27 -0300

    gerando novo build

--- dist/test.js
+++
@@ -1,7 +0,0 @@
-// src/test.ts
-import { decodeToken } from './utilsapi.js';
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-    'eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ikpva' +
-    'G4gRG9lIiwicm9sZSI6InVzZXIiLCJleHAiOjQ3NjI4OTcwMDAsImlhdCI6MTYyODk3MDAwMH0.' +
-    'TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ';
-console.log(decodeToken(token));
